7e70990e78e30179ab8bd4515973f7e21ece2136
Samuel <samuelmanalu1@gmail.com>
2019-05-31 20:38:58 +0700

    Deplotment Setup

--- src/index.js
+++ src/index.js
@@ -10,18 +10,28 @@ import * as serviceWorker from './serviceWorker';
 import LandingPages from './pages/LandingPages/LandingPages'
 
 
-ReactDOM.render(
-    <Router
-        basename= {process.env.PUBLIC_URL}
-    >
-      <div>
-        <Switch>
-          <Route exact path="/" component={LandingPages} />
-        </Switch>
-      </div>
+// ReactDOM.render(
+//     <Router
+//         basename= {process.env.PUBLIC_URL}
+//     >
+//       <div>
+//         <Switch>
+//           <Route exact path="/" component={LandingPages} />
+//         </Switch>
+//       </div>
+//     </Router>,
+//     document.getElementById("root")
+//   );
+  
+//   serviceWorker.unregister();
+  
+  ReactDOM.render(
+    <Router>
+        <div>
+          <Route exact path={process.env.PUBLIC_URL + '/'} component={LandingPages} />
+        </div>
     </Router>,
-    document.getElementById("root")
+    document.getElementById('root')
   );
   
-  serviceWorker.unregister();
-  
+  serviceWorker.unregister(); 
