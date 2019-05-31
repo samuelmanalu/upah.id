0492283ad150542b85df35918be0bf5cee66f0e6
Samuel <samuelmanalu1@gmail.com>
2019-05-31 21:14:26 +0700

    Deployment Setup

--- src/index.js
+++ src/index.js
@@ -9,28 +9,28 @@ import * as serviceWorker from './serviceWorker';
 // pages
 import LandingPages from './pages/LandingPages/LandingPages'
 
+// ReactDOM.render(
+//     <Router
+//         basename= {process.env.PUBLIC_URL}
+//     >
+//       <div>
+//         <Switch>
+//           <Route exact path="/" component={LandingPages} />
+//         </Switch>
+//       </div>
+//     </Router>,
+//     document.getElementById("root")
+//   );
+  
+//   serviceWorker.unregister();
+  
 ReactDOM.render(
-    <Router
-        basename= {process.env.PUBLIC_URL}
-    >
+  <Router>
       <div>
-        <Switch>
-          <Route exact path="/" component={LandingPages} />
-        </Switch>
+        <Route exact path={process.env.PUBLIC_URL + '/'} component={LandingPages} />
       </div>
-    </Router>,
-    document.getElementById("root")
-  );
-  
-  serviceWorker.unregister();
-  
-  // ReactDOM.render(
-  //   <Router>
-  //       <div>
-  //         <Route exact path={process.env.PUBLIC_URL + '/'} component={LandingPages} />
-  //       </div>
-  //   </Router>,
-  //   document.getElementById('root')
-  // );
-  
-  // serviceWorker.unregister(); 
+  </Router>,
+  document.getElementById('root')
+);
+
+  serviceWorker.unregister(); 
