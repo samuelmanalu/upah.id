30d4cca48cd27e7fb550daf9235e1a4ff0adfb61
Samuel <samuelmanalu1@gmail.com>
2019-05-31 21:27:08 +0700

    Deployment Setup

--- src/index.js
+++ src/index.js
@@ -24,13 +24,20 @@ import LandingPages from './pages/LandingPages/LandingPages'
   
 //   serviceWorker.unregister();
   
-ReactDOM.render(
-  <Router basename= {process.env.PUBLIC_URL}>
-      <div>
-        <Route exact path='/' component={LandingPages} />
-      </div>
-  </Router>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Router basename= {process.env.PUBLIC_URL}>
+//       <div>
+//         <Route exact path='/' component={LandingPages} />
+//       </div>
+//   </Router>,
+//   document.getElementById('root')
+// );
 
-  serviceWorker.unregister(); 
+// serviceWorker.unregister();
+
+ReactDOM.render(<LandingPages />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
